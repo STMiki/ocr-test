e60a8c3e73edb749b6ddb4911955403d076f991a
Michaël Veillet Guillem <mveillet@ponynov.fr>
2023-10-25 18:05:01 +0200

    Start recording trace

--- init-rasp.sh
+++ init-rasp.sh
@@ -6,6 +6,10 @@ echo "Installing dependencies"
 sudo apt install xorg-dev libudev-dev cmake clang libtesseract-dev vim git libopenal-dev libvorbis-dev libflac-dev libsfml-dev -y
 
 echo "Installing test app"
+if [ -d "ocr-test" ]; then
+    echo "Removing old test app"
+    rm -rf ocr-test
+fi
 git clone https://github.com/STMiki/ocr-test.git
 cd ocr-test
 mkdir -p build
@@ -16,6 +20,6 @@ cmake ..
 make
 
 echo "Running test app"
-./ocr-test
+./OcrTest
 
 echo "Done"
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@
 #include "SFML/Window/WindowStyle.hpp"
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <tesseract/baseapi.h>
+
+#define UNUSED __attribute__((unused))
+
+void move(UNUSED sf::Vector2i pos) { std::cout << "Moving to " << pos.x << ", " << pos.y << std::endl; }
 
 int main()
 {
+    tesseract::TessBaseAPI api = tesseract::TessBaseAPI();
     std::cout << "Hello, World!" << std::endl;
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+    bool pressed = false;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -14,12 +22,26 @@ int main()
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                 window.close();
             }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                std::cout << "Mouse Button Pressed" << std::endl;
+                pressed = true;
+            } else if (event.type == sf::Event::MouseButtonReleased) {
+                std::cout << "Mouse Button Released" << std::endl;
+                pressed = false;
+            } else if (event.type == sf::Event::MouseMoved) {
+                if (pressed)
+                    move({event.mouseMove.x, event.mouseMove.y});
+            }
+
             if (event.type == sf::Event::TouchBegan) {
                 std::cout << "Touch Began" << std::endl;
+                pressed = true;
             } else if (event.type == sf::Event::TouchEnded) {
                 std::cout << "Touch Ended" << std::endl;
+                pressed = false;
             } else if (event.type == sf::Event::TouchMoved) {
-                std::cout << "Touch Moved" << std::endl;
+                if (pressed)
+                    move({event.touch.x, event.touch.y});
             }
         }
 
