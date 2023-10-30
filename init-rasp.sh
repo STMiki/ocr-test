aa790e3fd4edba563bc243e23c6bcda2653301a1
Michaël Veillet Guillem <micou.veillet@gmail.com>
2023-10-30 13:14:26 +0100

    Fix init rasp script to install tessdata language file

--- init-rasp.sh
+++ init-rasp.sh
@@ -2,12 +2,34 @@
 
 set -e
 
-echo "** Installing dependencies"
+function info {
+    echo -e "\e[1;32m[INFO]\e[0m $1"
+}
+
+function error {
+    echo -e "\e[1;31m[ERROR]\e[0m $1"
+}
+
+function warning {
+    echo -e "\e[1;33m[WARNING]\e[0m $1"
+}
+
+function installLanguage {
+    if [ ! -f "/usr/share/tessdata/$1.traineddata" ]; then
+        info "Installing $1 language"
+        sudo wget https://github.com/tesseract-ocr/tessdata/raw/main/$1.traineddata -o /usr/share/tessdata/$1.traineddata
+    fi
+}
+
+info "** Installing dependencies"
 sudo apt install xorg-dev libudev-dev cmake clang libtesseract-dev vim git libopenal-dev libvorbis-dev libflac-dev libsfml-dev -y
 
-echo "** Installing test app"
+installLanguage eng
+installLanguage fra
+
+info "** Installing test app"
 if [ -d "ocr-test" ]; then
-    echo "Removing old test app"
+    warning "Removing old test app"
     rm -rf ocr-test
 fi
 git clone https://github.com/STMiki/ocr-test.git
@@ -15,11 +37,11 @@ cd ocr-test
 mkdir -p build
 cd build
 
-echo "** Building test app"
+info "** Building test app"
 cmake ..
 make
 
-echo "** Running test app"
+info "** Running test app"
 DISPLAY=:0 ./OcrTest
 
-echo "** Done"
+info "** Done"
