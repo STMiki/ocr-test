f13c83430f70f04387528dbd41c5c1fd2c38b87e
Michaël Veillet Guillem <micou.veillet@gmail.com>
2023-10-30 23:35:04 +0100

    Install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#/usr/bin/env bash
+
+set -e
+
+function info {
+    echo -e "\e[1;32m[INFO]\e[0m $1"
+}
+
+function warning {
+    echo -e "\e[1;33m[WARNING]\e[0m $1"
+}
+
+function error {
+    echo -e "\e[1;31m[ERROR]\e[0m $1"
+}
+
+function fatal {
+    echo -e "\e[1;31m[FATAL]\e[0m ** $1 **"
+    exit 1
+}
+
+info "Installing test app"
+cp build/OcrTest ~/Desktop
+
+info "Done"
